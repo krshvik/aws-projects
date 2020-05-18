22ec6fc37c7b4939e8a4e2a56b3f651bd8a0c229
krshvik <krsh.vik@gmail.com>
2020-05-18 13:37:45 -0500

    Create sqs_functions.py

---
+++ sqs_functions.py
@@ -0,0 +1,15 @@
+import boto3
+sqs = boto3.client('sqs')
+
+## an error occurs if you try to create a queue with the same name without waiting 60 seconds
+
+try:
+    queue_name = 'test-queue2'
+    response = sqs.create_queue(QueueName=queue_name)
+    print(response['QueueUrl'])
+    qurl= response['QueueUrl']
+    response = sqs.delete_queue(QueueUrl=qurl)
+    print(response)
+except Exception as e:
+    print(e)
+
