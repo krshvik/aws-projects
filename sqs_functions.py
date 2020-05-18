cf8fa1cd266bbbdbd56f4f882a748879e5832e23
krshvik <krsh.vik@gmail.com>
2020-05-18 13:42:15 -0500

    Update sqs_functions.py

--- sqs_functions.py
+++ sqs_functions.py
@@ -1,12 +1,12 @@
 import boto3
 sqs = boto3.client('sqs')
 
-## an error occurs if you try to create a queue with the same name without waiting 60 seconds
+## an error occurs if you try to create a queue with the same name without waiting 60 seconds after deleting it
 
 try:
     queue_name = 'test-queue2'
     response = sqs.create_queue(QueueName=queue_name)
-    print(response['QueueUrl'])
+    print(response['QueueUrl'],response['ResponseMetadata']['HTTPStatusCode'])
     qurl= response['QueueUrl']
     response = sqs.delete_queue(QueueUrl=qurl)
     print(response)
