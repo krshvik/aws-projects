f6f394696e95af6ffcc5e9ed4684ad6af4ef1478
Vikram Repaka <vikram.repaka@garmin.com>
2020-05-28 12:15:10 -0500

    Update s3-list-buckets.py

--- s3-list-buckets.py
+++ s3-list-buckets.py
@@ -1,5 +1,5 @@
-### list all s3 buckets
-### upload a file to s3 bucket
+### list all s3 buckets and their contents
+### upload a file to s3 bucket - remove comments to upload a file
 
 import boto3
 import urllib3
@@ -13,11 +13,22 @@ response = s3.list_buckets()
 bucks = response['Buckets']
 
 if len(bucks) > 0:
-    buck  = bucks[0]['Name']
-    cdir = os.path.abspath(os.getcwd())
-    print(cdir)
-    f = os.path.join(cdir,'cb.jpg')
-    ### upload a file
-    print(f)
-    s3 = boto3.resource('s3',verify=False)
-    s3.meta.client.upload_file(f,buck,'cb2.jpg')
+
+    lenb = len(bucks)
+    i = 0
+    buck = ''
+    while i < lenb:
+        buck  = bucks[i]['Name']
+        print(buck)
+        response = s3.list_objects(Bucket=buck)
+        contents = response['Contents']
+        for c in contents:
+            print(c['Key'],c['Size'],c['StorageClass'])
+        i = i + 1
+    # cdir = os.path.abspath(os.getcwd())
+    # print(cdir)
+    # f = os.path.join(cdir,'cb.jpg')
+    # ### upload a file
+    # print(f)
+    # s3 = boto3.resource('s3',verify=False)
+    # s3.meta.client.upload_file(f,buck,'cb2.jpg')
